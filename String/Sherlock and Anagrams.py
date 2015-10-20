5af2a7eea627f84ec0d4877d4d6fa8ad07747079
Holic <soundholicc@gmail.com>
2015-10-20 22:59:51 +0800

    update
    虽然没有仔细看但是这个写法比我的效率高了大概10倍左右

--- String/Sherlock and Anagrams.py
+++ String/Sherlock and Anagrams.py
@@ -1,9 +1,13 @@
 def solution(s):
 	count = 0
-	l1=0,r1=1,l2=1,r2=2
-	if sorted(s[l1,r1]) == sorted(s[l2,r2]):
-			
-	
+	lng = len(s)
+	r1,r2 = 1,2
+	for range in xrange(1,lng):
+		for i in xrange(lng-range):
+			for j in xrange(i+1,lng +1 - range):
+				if sorted(s[i:i+range]) == sorted(s[j:j+range]):
+					count +=1
+	return count
 
 if __name__ == '__main__':
 	T = int(raw_input())
